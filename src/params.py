cbef5e7493967f56ef6c5492c98f1eb32ca09a01
Willem Adnet <willem.adnet@grenoble-inp.org>
2025-11-09 00:25:59 +0100

    feat: new map parameters

---
+++ src/params.py
@@ -0,0 +1,10 @@
+"""Param file that handle all the parameters."""
+
+# File
+FILE = "data/MN_01_2000-2009.csv"
+
+# Limits:
+LIMIT_WEST = "005 48 W"
+LIMIT_EST = "10 E"
+LIMIT_NORTH = "51 30 N"
+LIMIT_SOUTH = "41 N"
